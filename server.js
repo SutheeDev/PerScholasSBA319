402d5abf8623129c10409d3a02139a5491111b37
SU-T <guitaristpeet@gmail.com>
2025-01-12 20:17:21 -0500

    imported and invoked conn

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const conn = require("./config/db");
+conn();
 
 app.get("/", (req, res) => {
   res.send("Welcome!");
