bc78961855d087d1aa387b0b15481b1610459f02
SU-T <guitaristpeet@gmail.com>
2025-01-14 22:01:01 -0500

    created searchRstaurant route

--- routes/restaurantRoute.js
+++ routes/restaurantRoute.js
@@ -28,4 +28,34 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// searchRestaurant route
+router.post("/search", async (req, res) => {
+  try {
+    const { name, cuisine, location } = req.body;
+
+    let queryArr = [];
+    if (name) {
+      queryArr.push({ name });
+    }
+    if (cuisine) {
+      queryArr.push({ cuisine });
+    }
+    if (location) {
+      queryArr.push({ location });
+    }
+
+    // https://www.mongodb.com/docs/manual/reference/operator/query/or/
+    // use MongoDB $or operator to return result with any of these queries
+    const result = await Restaurant.find({ $or: queryArr });
+
+    if (result.length === 0) {
+      return res.status(400).json("No restaurant matches this search");
+    }
+
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 module.exports = router;
